9f3d2c4deed4b817d691daf6fdaf3e28411c6eb9
Karinna Iniguez <karinnainiguez@gmail.com>
2018-05-21 15:15:46 -0700

    can console log a string when clicking on each note button

--- noise.js
+++ noise.js
@@ -1,3 +1,9 @@
 $(document).ready( function() {
   // your code here
+  $('.instrument').on('click','button',function(){
+    console.log('something');
+    $('source').each(function(){
+      this.play
+    });
+  });
 });
