5311fce44420133bc5deabc30172d339ee8d56ef
Karinna Iniguez <karinnainiguez@gmail.com>
2018-05-22 10:08:25 -0700

    Add functionality for keydown and click

--- noise.js
+++ noise.js
@@ -1,9 +1,36 @@
-$(document).ready( function() {
-  // your code here
-  $('.instrument').on('click','button',function(){
-    console.log('something');
-    $('source').each(function(){
-      this.play
-    });
+
+const player = function player(target){
+  $(target).each( function() {
+    this.pause();
+    this.currentTime = 0
+    this.play();
   });
+}
+
+$(document).ready( function() {
+
+  $('.instrument').on('click','button',function(event){
+    let button = event.target.classList[1];
+    let target = `#${button}Audio`;
+
+    player(target);
+  });
+
+
+  $('body').keydown(function(event){
+    let note = event.key.toLowerCase();
+    let allowed = ['a','b','c','d','e', 'f','g'];
+
+    if (allowed.includes(note)) {
+
+      let target = `#${note}Audio`;
+      $(`.${note}`).addClass('active');
+
+      player(target);
+
+      setTimeout(function(){ $(`.${note}`).removeClass('active'); }, 150);
+    }
+
+  });
+
 });
